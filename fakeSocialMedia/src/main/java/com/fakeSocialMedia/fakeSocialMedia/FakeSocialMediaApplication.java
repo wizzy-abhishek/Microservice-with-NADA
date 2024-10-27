ec9a7e8a64a1ccc4a45e58039f8f2f79c15e7773
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2024-10-27 13:00:57 +0530

    Test

--- fakeSocialMedia/src/main/java/com/fakeSocialMedia/fakeSocialMedia/FakeSocialMediaApplication.java
+++ fakeSocialMedia/src/main/java/com/fakeSocialMedia/fakeSocialMedia/FakeSocialMediaApplication.java
@@ -8,7 +8,7 @@ public class FakeSocialMediaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FakeSocialMediaApplication.class, args);
-		System.out.println("Hello FAKE SOCIAL MEDIA");
+		System.out.println("Hello FAKE SOCIAL MEDIA ");
 	}
 
 }
